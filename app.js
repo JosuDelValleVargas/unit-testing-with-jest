0e26ab51d1aa1178e0e1e0880d0681581e256ca0
Josu DelValleVargas <josudelvallevargas@gmail.com>
2023-04-10 19:14:08 +0000

    entrega del proyecto

---
+++ app.js
@@ -0,0 +1,39 @@
+/*
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum };
+*/
+
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+// funcion para convertir de euro a dolar
+
+const fromEuroToDollar = function (euro){
+    return euro * oneEuroIs.USD  ;
+}
+
+const fromDollarToYen = function (dollar){
+    return dollar* oneEuroIs.JPY ;
+}
+
+const fromYenToPound = function (yen){
+    return yen* oneEuroIs.GBP ;
+}
+
+console.log(fromEuroToDollar());
+console.log(fromDollarToYen());
+console.log(fromYenToPound());
+
+module.exports = { fromEuroToDollar,fromDollarToYen,fromYenToPound };
